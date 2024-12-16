d36d03bf58c14c3a1f59312bf11402d4c685e1b9
AliHussnian35 <AliHussnain5143@gmail.com>
2024-12-16 18:06:13 +0500

    ConversionOfDataTypes

---
+++ ConversionOperations.js
@@ -0,0 +1,10 @@
+let score = true
+ 
+console.log(score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(score)
+// "123abc" => Nan
+// true => 1; false => 0;

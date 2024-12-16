b827b93eef41633e3a6312487645089136320573
AliHussnian35 <AliHussnain5143@gmail.com>
2024-12-16 16:37:36 +0500

    Variables

--- Test.js
+++ Test.js
@@ -1,1 +1,13 @@
-console.log("Ali")
+const accountId = 12345
+let accountEmail = "ali@gmail.com"
+var accountPassword = "123456"
+accountCity = "Lahore"
+
+
+accountEmail = "a@gmail.com"
+accountPassword = "123"
+accountCity = "Multan"
+// accountId = 2
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
